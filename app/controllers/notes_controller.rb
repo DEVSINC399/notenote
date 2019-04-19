6a6c89d79102a02905a0d38b9002dfc7b09f8042
DEVSINC399 <ertiza.ejaz@devsinc.com>
2019-04-19 13:06:49 +0500

    Devise added and Configured + Alternated root_path set

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,15 +1,16 @@
 class NotesController < ApplicationController
     before_action :find_note, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!
     def index
-        @notes = Note.all.order("created_at desc")
+        @notes = current_user.notes.order("created_at desc")
     end
 
     def new
-        @note = Note.new
+        @note = current_user.notes.new
     end
     
     def create
-        @note = Note.new(note_params)
+        @note = current_user.notes.new(note_params)
         if @note.save
             redirect_to @note, alert: "Record Saved Successfully."
         else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/welcome', to: 'welcome#index' 
+
+  authenticated :user do
+    root 'notes#index', as: 'authenticated_root'
+  end
   root 'welcome#index'
 
   resources :notes
